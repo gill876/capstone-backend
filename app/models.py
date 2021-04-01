996de57a1f5167e5c52e51bdc5af447043a719fa
Cargill Seiveright <microcargill@hotmail.com>
2021-04-01 02:22:34 -0500

    Initial Model

--- app/models.py
+++ app/models.py
@@ -2,19 +2,14 @@ from . import db
 from werkzeug.security import generate_password_hash
 
 
-class UserProfile(db.Model):
-    __tablename__ = 'user_profiles'
+class User(db.Model):
+    __tablename__ = 'user'
 
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(80))
-    last_name = db.Column(db.String(80))
     username = db.Column(db.String(80), unique=True)
     password = db.Column(db.String(255))
-    type = db.Column(db.Integer)
 
-    def __init__(self, first_name, last_name, username, password):
-        self.first_name = first_name
-        self.last_name = last_name
+    def __init__(self, username, password):
         self.username = username
         self.password = generate_password_hash(password, method='pbkdf2:sha256')
 
@@ -35,3 +30,71 @@ class UserProfile(db.Model):
 
     def __repr__(self):
         return '<User %r>' % (self.username)
+
+
+class Profile(db.Model):
+    __tablename__ = 'profile'
+
+    id = db.Column(db.Integer, primary_key=True)
+    phone_id = db.Column(db.String(255), unique=True)
+    gender = db.Column(db.Integer)
+    extraversion = db.Column(db.Integer)
+    agreeableness = db.Column(db.Integer)
+    conscientiousness = db.Column(db.Integer)
+    emotional_stability = db.Column(db.Integer)
+    intellect_imagination = db.Column(db.Integer)
+
+    def __init__(self, phone_id, gender):
+        self.phone_id = phone_id
+        self.gender = gender
+
+
+class Phone(db.Model):
+    __tablename__ = 'phone'
+
+    id = db.Column(db.Integer, primary_key=True)
+    phone_id = db.Column(db.String(255), unique=True)
+    os_version = db.Column(db.String(50))
+    cpu_utilization = db.Column(db.Integer)
+    memory_utilization = db.Column(db.Integer)
+    running_apps = db.Column(db.Integer)
+    battery_percentage = db.Column(db.Integer)
+    battery_state = db.Column(db.Integer)
+    phone_model = db.Column(db.String(50))
+    timestamp = db.Column(db.DateTime)
+
+
+class UserPhone(db.Model):
+    """Connects User and Phone."""
+
+    __tablename__ = 'user_xphone'
+
+    phone_id = db.Column(db.String(255))
+    username = db.Column(db.String(80))
+
+
+class AppPackageCategory(db.Model):
+    __tablename__ = 'app_package_category'
+
+    id = db.Column(db.Integer, primary_key=True)
+    app_name = os_version = db.Column(db.String(100))
+    package_name = os_version = db.Column(db.String(100))
+    package_name = os_version = db.Column(db.String(50))
+
+
+class RunningApps(db.Model):
+    """Running Apps from Phone table connecting to App Pkg. Cat.."""
+
+    __tablename__ = "running_apps"
+    running_apps = db.Column(db.Integer)
+    app_pkg_cat = db.Column(db.Integer)
+
+class AppUsage(db.Model):
+    """App Usage for user."""
+
+    __tablename__ = "app_usage"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80))
+    app_pkg_cat = db.Column(db.Integer)
+    time_sec = db.Column(db.Integer)
