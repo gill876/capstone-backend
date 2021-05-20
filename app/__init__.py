edcc7f9c815224ef2571c7aafcc554b371b606b8
Cargill Seiveright <microcargill@hotmail.com>
2021-05-20 03:46:45 -0500

    Remove HTTP Auth

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_httpauth import HTTPBasicAuth
 from .config import Config
 from flask_cors import CORS
 from dotenv import load_dotenv
@@ -9,14 +8,10 @@ load_dotenv()
 
 app = Flask(__name__)
 
-
 CORS(app)
-# app.config["CORS_HEADERS"] = "Content-Type"
 
 app.config.from_object(Config)
 
 db = SQLAlchemy(app)
-
-auth = HTTPBasicAuth(scheme='Bearer')
 
 from app import views
